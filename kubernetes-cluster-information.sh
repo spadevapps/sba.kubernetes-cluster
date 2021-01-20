ada8e54f087caaa2833905e626c3e7a2425b7cc4
spadevaps <spappl19@g.holycross.edu>
2021-01-19 20:15:25 -0500

    Made script output easier to read

--- kubernetes-cluster-information.sh
+++ kubernetes-cluster-information.sh
@@ -1,6 +1,23 @@
+echo "Deployment info"
+echo
 kubectl get deployments
+echo
 kubectl describe deployment sba-deployment
+echo
+echo
+echo
+echo "Pod Info"
+echo
 kubectl get pods
+echo
+kubectl get pods -o wide
+echo
 kubectl describe pods
+echo
+echo
+echo
+echo "Sercive Info"
+echo
 kubectl get services
+echo
 kubectl describe service
